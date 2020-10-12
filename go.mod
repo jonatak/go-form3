4787a865492504e819bec1315329b3fb4574e7e4
Jonathan Billaud <jonathan.billaud@gmail.com>
2020-10-12 19:19:04 +0200

    chore: add default case for country validation and extra documentation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jonatak/go-form3
 
 go 1.15
+
+require github.com/prometheus/common v0.14.0
--- pkg/account/countries.go
+++ pkg/account/countries.go
@@ -1,6 +1,10 @@
 package account
 
-import "errors"
+import (
+	"errors"
+
+	"log"
+)
 
 // Country define a specific type for country for further validation.
 type Country string
@@ -28,10 +32,19 @@ const (
 var ErrInvalidCountry = errors.New("Submitted country is invalid")
 
 // IsValid validate that a country is in the range of supported code.
+// Note for reviewer: I assumed that the list of countries listed in the documentation
+// are the only supported one, I guess that having an extra library here to validate the country
+// code on the client side could be a nice to have, didn't include any here to match the specs of the
+// take home exercise.
 func (c Country) IsValid() error {
 	switch c {
 	case GB, AU, BE, CA, FR, GR, HK, IT, LU, NL, PL, PT, ES, CH, US:
 		return nil
+	default:
+		if len(c) == 2 { // Allow other country code to not tied client library and server too much.
+			log.Printf("Country code %s isn't in the list of supported countries", c)
+			return nil
+		}
 	}
 	return ErrInvalidCountry
 }
