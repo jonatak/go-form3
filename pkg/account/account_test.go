d9011d40d7db6badc135e2f2d057099852597a0f
Jonathan Billaud <jonathan.billaud@gmail.com>
2020-10-18 11:55:14 +0200

    chore: switch back to blackbox testing for account package

--- pkg/account/account_test.go
+++ pkg/account/account_test.go
@@ -1,13 +1,11 @@
-//+build unit
-//+build !integration
-
-package account
+package account_test
 
 import (
 	"errors"
 	"fmt"
 	"testing"
 
+	"github.com/jonatak/go-form3/pkg/account"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -18,34 +16,34 @@ func assertReturnedError(t *testing.T, got, want error) {
 func TestAccountValidation(t *testing.T) {
 
 	t.Run("Account validation without country should be invalid", func(t *testing.T) {
-		ar := &Account{}
-		assertReturnedError(t, ar.IsValid(), ErrFieldMandatory)
+		ar := &account.Account{}
+		assertReturnedError(t, ar.IsValid(), account.ErrFieldMandatory)
 	})
 
 	t.Run("Account validation without country of 3 char should be invalid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country: "FRA",
 		}
-		assertReturnedError(t, ar.IsValid(), ErrInvalidCountry)
+		assertReturnedError(t, ar.IsValid(), account.ErrInvalidCountry)
 	})
 
 	t.Run("Account validation with valid country shouldn't return error", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country: "FR",
 		}
 		assertReturnedError(t, ar.IsValid(), nil)
 	})
 
 	t.Run("Base currency with more than 3 char should be invalid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:      "FR",
 			BaseCurrency: "EURO",
 		}
-		assertReturnedError(t, ar.IsValid(), ErrFieldInvalid)
+		assertReturnedError(t, ar.IsValid(), account.ErrFieldInvalid)
 	})
 
 	t.Run("Base currency EUR should be valid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:      "FR",
 			BaseCurrency: "EUR",
 		}
@@ -53,15 +51,15 @@ func TestAccountValidation(t *testing.T) {
 	})
 
 	t.Run("Base currency with more than 3 char should be invalid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:      "FR",
 			BaseCurrency: "EURO",
 		}
-		assertReturnedError(t, ar.IsValid(), ErrFieldInvalid)
+		assertReturnedError(t, ar.IsValid(), account.ErrFieldInvalid)
 	})
 
 	t.Run("Base currency EUR should be valid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:      "FR",
 			BaseCurrency: "EUR",
 		}
@@ -69,16 +67,16 @@ func TestAccountValidation(t *testing.T) {
 	})
 
 	t.Run("BankID with more than 11 char is invalid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:      "FR",
 			BaseCurrency: "EUR",
 			BankID:       "1234567890av",
 		}
-		assertReturnedError(t, ar.IsValid(), ErrFieldBankIDInvalidLength)
+		assertReturnedError(t, ar.IsValid(), account.ErrFieldBankIDInvalidLength)
 	})
 
 	t.Run("BankID with less than 11 char is invalid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:      "FR",
 			BaseCurrency: "EUR",
 			BankID:       "1234567890",
@@ -89,7 +87,7 @@ func TestAccountValidation(t *testing.T) {
 
 func TestClassificationTypeValidation(t *testing.T) {
 	t.Run("Personal Classification type is valid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:               "FR",
 			BaseCurrency:          "EUR",
 			BankID:                "1234567890",
@@ -99,7 +97,7 @@ func TestClassificationTypeValidation(t *testing.T) {
 	})
 
 	t.Run("Business Classification type is valid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:               "FR",
 			BaseCurrency:          "EUR",
 			BankID:                "1234567890",
@@ -109,19 +107,19 @@ func TestClassificationTypeValidation(t *testing.T) {
 	})
 
 	t.Run("Other Classification type is valid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:               "FR",
 			BaseCurrency:          "EUR",
 			BankID:                "1234567890",
 			AccountClassification: "Other",
 		}
-		assertReturnedError(t, ar.IsValid(), ErrInvalidAccountClassificationType)
+		assertReturnedError(t, ar.IsValid(), account.ErrInvalidAccountClassificationType)
 	})
 }
 
 func TestStatusValidation(t *testing.T) {
 	t.Run("pending status is valid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:               "FR",
 			BaseCurrency:          "EUR",
 			BankID:                "1234567890",
@@ -132,7 +130,7 @@ func TestStatusValidation(t *testing.T) {
 	})
 
 	t.Run("confirmed status is valid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:               "FR",
 			BaseCurrency:          "EUR",
 			BankID:                "1234567890",
@@ -143,7 +141,7 @@ func TestStatusValidation(t *testing.T) {
 	})
 
 	t.Run("failed status is valid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:               "FR",
 			BaseCurrency:          "EUR",
 			BankID:                "1234567890",
@@ -154,13 +152,13 @@ func TestStatusValidation(t *testing.T) {
 	})
 
 	t.Run("other status is valid", func(t *testing.T) {
-		ar := &Account{
+		ar := &account.Account{
 			Country:               "FR",
 			BaseCurrency:          "EUR",
 			BankID:                "1234567890",
 			AccountClassification: "Personal",
 			Status:                "other",
 		}
-		assertReturnedError(t, ar.IsValid(), ErrInvalidAccountStatus)
+		assertReturnedError(t, ar.IsValid(), account.ErrInvalidAccountStatus)
 	})
 }
